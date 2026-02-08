b6e723eeb62f94928e97761254855387e14be524
Francis Omondi <francisomondi17@gmail.com>
2026-02-08 13:51:50 +0300

    hero, and backend controllers updated

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
+app.set("trust proxy", 1);
 
 // API routes
 app.use("/api/donation", donationRoutes);
