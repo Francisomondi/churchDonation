dd5052009913552e2615d56eb3abf648cf530c74
Francis Omondi <francisomondi17@gmail.com>
2026-03-13 11:38:52 +0300

    user login, mobile view

--- frontend/src/components/DonationTypes.jsx
+++ frontend/src/components/DonationTypes.jsx
@@ -7,7 +7,7 @@ const donations = [
 
 export default function DonationTypes() {
   return (
-    <section className="bg-gray-100 py-16 px-6">
+    <section className="bg-gray-100 py-16 px-6 text-emerald-600">
       <h3 className="text-3xl text-center font-semibold mb-10">
         Ways to Give
       </h3>
